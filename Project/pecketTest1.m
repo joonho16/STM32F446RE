ef6ba35c5f28d52ab8b5502d56fe9d18e27d860f
rhwns <rhwns@DESKTOP-O8LPEUK>
2023-09-19 21:27:40 +0900

    ADC 2개 테스트 완료
    DMA 설정을 Normal -> Circular로 바꿔서 ADC 변환이 계속되도록 설정

--- Project/pecketTest1.m
+++ Project/pecketTest1.m
@@ -4,6 +4,12 @@ clc;
 %% Pecket
 
 header = [0xFF 0xFF];
-data_length = 0x02;
-length = 0x02 + 0x01 + data_length + 0x01;
 tail   = 0x0A;
+
+data_unit_length = 0x02;
+data_number = 2;
+
+length = 0x02                           ...; % header
+       + 0x01                           ...; % length
+       + data_unit_length * data_number ...; % data
+       + 0x01;                               % tail   
